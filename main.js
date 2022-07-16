0dd7fa2fda4cb528e39166cccb1e9b4e9833e11c
Biplob Molla <ahmedtamim317@gmail.com>
2022-07-16 16:04:20 +0600

    Home Page responsive

---
+++ main.js
@@ -0,0 +1,11 @@
+var navMenuIcon = document.querySelector("#nav__menu__icon");
+var navMenuCloseIcon = document.querySelector("#nav__menu__close__icon");
+var menu = document.querySelector(".menu");
+
+navMenuIcon.addEventListener("click", () => {
+    menu.style.right = "0";
+});
+
+navMenuCloseIcon.addEventListener("click", () => {
+    menu.style.right = "-200%";
+});
