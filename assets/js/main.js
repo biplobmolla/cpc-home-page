58d00d85f0852e292e16a06cc362791051a999fb
Biplob Molla <ahmedtamim317@gmail.com>
2022-07-20 23:13:14 +0600

    Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,16 @@ navMenuIcon.addEventListener("click", () => {
 navMenuCloseIcon.addEventListener("click", () => {
     menu.style.right = "-200%";
 });
+
+var selecListShowBtn = document.querySelectorAll(".selected__value .fa-sort-down");
+var selectCloseBtn = document.querySelectorAll(".select__menu__close__btn");
+var selectMenuList = document.querySelectorAll(".pop__up__menu");
+
+for (let i = 0; i < selectMenuList.length; i++) {
+    selecListShowBtn[i].addEventListener("click", () => {
+        selectMenuList[i].style = "display: block; opacity: 1; transform: translateY(0);";
+    });
+    selectCloseBtn[i].addEventListener("click", () => {
+        selectMenuList[i].style = "opacity: 0; transform: translateY(-50%); display: none;";
+    });
+}
